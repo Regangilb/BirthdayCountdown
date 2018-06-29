88a0eec45f362b230ac138beed48796edd82d27f
Regan Gilbert <regan.m.gilbert@gmail.com>
2018-06-29 13:34:54 -0600

    large text label and keys

--- src/components/app.js
+++ src/components/app.js
@@ -31,17 +31,15 @@ export default class App extends Component {
 
   handleChange = function(date) {
     console.log("APP JS HANDLE CHANGE", date._d);
-    clearInterval(this.timer);
+   
     this.setState({
       startDate: date
     });
   }.bind(this);
 
   handleGenerate = function() {
-
-    
-
-
+    clearInterval(this.timer)
+   
     var bday = this.state.startDate.toDate();
     var today = new Date();
     var currentMonth = today.getMonth();
@@ -50,41 +48,34 @@ export default class App extends Component {
     var timeBetween = today.getTime() - bday.getTime();
     var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
     var age = Number((daysOld/365).toFixed(0));
-    this.setState({
+    this.setState({ 
       age,
-      active:true
+      active: true
     })
-
 
     if(birthMonth > currentMonth) {
       bday.setFullYear(today.getFullYear())
-    } else if (birthMonth < currentMonth) {
+    } else if(birthMonth < currentMonth) {
       bday.setFullYear(today.getFullYear() + 1)
     } else if(birthMonth == currentMonth) {
       var currentDay = today.getDate();
       var birthDay = bday.getDate();
 
       if(birthDay > currentDay) {
-        bday.setFullYear(today.getFullYear() + 1)
+        bday.setFullYear(today.getFullYear())
       }
       if(birthDay <= currentDay) {
-        bday.setFullYear(today.getFullYear() +1)
+        bday.setFullYear(today.getFullYear() + 1)
       }
     }
-    
- 
 
     var countDownDate = bday.getTime();
 
-
     this.timer = setInterval(function() {
 
-      var now = new Date().getTime();
+      var now = moment().toDate().getTime();
       var distance = countDownDate - now;
 
-      var distance = countDownDate - now;
-
-      
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -92,7 +83,6 @@ export default class App extends Component {
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      
       const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       const timeRemaining = {
         days,
@@ -100,25 +90,32 @@ export default class App extends Component {
         minutes,
         seconds
       };
+      
       this.setState({ timeRemaining });
 
-      console.log(this.state.timeRemaining);
-
-     
       if (distance < 0) {
         clearInterval(this.timer);
-   
+        // document.getElementById("demo").innerHTML = "EXPIRED";
       }
     }.bind(this), 1000);
+  }.bind(this);
+
+  getBirthDate = function(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    if(month < 10) {
+      return `0${month}/${day}`
+    }
+    return `${month}/${day}`
   }.bind(this);
 
   renderItems = function() {
     if (this.state.active) {
       return [
-        <Clock timeRemaining={this.state.timeRemaining} />,
+        <Clock key={0} timeRemaining={this.state.timeRemaining} />,
         ChangeDate("Change Date", () => this.setState({ active: false })),
-        LargeText("04/03"),
-        <label className="grid__remaining">
+        LargeText(this.getBirthDate(this.state.startDate.toDate())),
+        <label key={3} className="grid__remaining">
           Remaining until you turn {this.state.age}
         </label>
       ];
@@ -127,6 +124,7 @@ export default class App extends Component {
         <Picker
           startDate={this.state.startDate}
           callback={date => this.handleChange(date)}
+          key={0}
         />,
         Button("Generate Countdown", () => this.handleGenerate())
       ];
--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const Button = (title, callback) => {
-        return (
-            <button className="button" onClick= {callback}>
-                {title}
-            </button> 
-
-        )
-    }
+    return (
+        <button key={1} className="button" onClick={callback}>
+            {title}
+        </button>
+    )
+}
 
 export default Button;
