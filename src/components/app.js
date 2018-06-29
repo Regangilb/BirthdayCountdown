f823575601c596c1065692628a73a95752d6b0db
Regan Gilbert <regan.m.gilbert@gmail.com>
2018-06-29 10:25:23 -0600

    added large text and remaining label

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import ChangeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
 
@@ -14,35 +15,39 @@ export default class App extends Component {
     }
   }
 
+
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock/>, 
-        ChangeDate('Change Date', () => this.setState({ active: false}))
+        <Clock/>,
+        ChangeDate('Change Date', () => this.setState({ active: false })),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your 21st birthday</label>
       ]
     } else {
-      return Button ('Generate Countdown',  () => this.setState({ active: true }))
+      return [
+        <Picker/>,
+        Button('Generate Countdown', () => this.setState({ active: true }))
+      ]
     }
-
   }.bind(this)
-
 
   render() {
     return (
       <div className="grid">
-      <h2 className="grid__title"> Birthday Countdown</h2>
-      <div className="grid__skew-dark-one-box"></div>
-      <div className="grid__skew-dark-two"></div>
-      <div className="grid__skew-dark-three"></div>
+        <h1 className="grid__title">Birthday Countdown</h1>
 
-      <div className="grid__skew-light-one"></div>
-      <div className="grid__skew-light-two"></div>
-      <div className="grid__skew-light-three-box"></div>
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
 
-      <Picker/>
-      { this.renderItems() }
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
+
+        { this.renderItems() }
       
-        
+  
       </div>
     );
   }
